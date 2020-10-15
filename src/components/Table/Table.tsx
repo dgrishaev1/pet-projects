c08aa9ce3546c222983d2f6db8a28972f059d7bc
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2020-10-15 16:05:23 +0300

    Структура components+containers, Main, Table, удаление лишнего

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <h5>I'm a table component</h5>
+  )
+}
+
+export default Table
---
+++ src/containers/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Table from '../../components/Table/Table'
+
+function Main() {
+  return (
+    <div>
+      <h2>I'm a main container</h2>
+      <Table/>
+    </div>
+  )
+}
+
+export default Main
