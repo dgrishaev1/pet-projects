eec9b34a8591b9f43e201c37dcdb071a9cb7150f
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2020-10-15 19:27:22 +0300

    Добавил router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Main from './containers/Main/Main';
+import {Switch, Redirect, Route} from 'react-router-dom';
 
 const App: React.FC = () => {
+  let routes = (
+    <Switch>
+      <Route path="/" exact component={Main}/>
+      <Redirect to="/"/>
+    </Switch>
+  )
+
   return (
     <div>
-      <Main/>
+      {routes}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
