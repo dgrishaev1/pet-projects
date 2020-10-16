9f1c669301bf6c95e83c255accb6042a09c970d4
Roman Ignanin <->
2020-10-16 12:32:50 +0300

    Форматирование

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function Table() {
   return (
     <h5>I'm a table component</h5>
-  )
+  );
 }
 
-export default Table
+export default Table;
