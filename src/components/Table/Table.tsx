1184c26a0816d76daa95cf5177a60a634ad46c54
Roman Ignanin <->
2020-10-16 12:41:35 +0300

    Мелкие исправления

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Table() {
+function Table(): React.ReactElement {
   return (
-    <h5>I'm a table component</h5>
+    <h5>I&apos;m a table component</h5>
   );
 }
 
