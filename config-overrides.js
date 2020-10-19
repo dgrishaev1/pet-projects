70835e7da00ac625d735cb151393cf1e02f86a09
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2020-10-19 12:02:04 +0300

    Добавлены alias, redux, структура

--- config-overrides.js
+++ config-overrides.js
@@ -9,6 +9,10 @@ module.exports = {
       ...config.resolve,
       alias: {
         '@controller': path.resolve(__dirname, 'src/controller'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@containers': path.resolve(__dirname, 'src/containers'),
+        '@actions': path.resolve(__dirname, 'src/actions'),
+        '@reducers': path.resolve(__dirname, 'src/reducers'),
       },
     };
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
-import { Test } from '@controller/test';
-
-import Main from './containers/Main/Main';
+import Main from '@containers/Main/Main';
 
 const App: React.FC = () => {
-  Test();
   return (
     <Switch>
       <Route path="/" exact component={Main} />
