93a58ec459280bdd4feefa920a5e4bd96e7b35c7
Vladislav Pavlov <->
2020-10-19 10:39:57 +0300

    add alias config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Switch, Redirect, Route} from 'react-router-dom';
+import { Switch, Redirect, Route } from 'react-router-dom';
+
+import { Test } from '@controller/test';
 
 import Main from './containers/Main/Main';
 
 const App: React.FC = () => {
+  Test();
   return (
     <Switch>
-      <Route path="/" exact component={Main}/>
-      <Redirect to="/"/>
+      <Route path="/" exact component={Main} />
+      <Redirect to="/" />
     </Switch>
   );
 };
