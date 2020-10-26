6f9cd6da9cccf990b92deab28cecd39a78c628b4
Roman Ignanin <->
2020-10-26 18:11:06 +0300

    Создал заготовку для меню

---
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,9 @@
+//import Menu from '@material-ui/core/Menu';
+//import MenuItem from '@material-ui/core/MenuItem';
+import React from 'react';
+
+export const ContextMenu: React.FC = () => {
+  return (
+    <div>5</div>
+  );
+};
