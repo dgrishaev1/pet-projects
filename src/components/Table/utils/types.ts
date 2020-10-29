1ec4c68b2ade9ad54af227c732d055f1e46467d2
Roman Ignanin <->
2020-10-29 15:37:14 +0300

    export нового типа json

--- src/components/Table/utils/types.ts
+++ src/components/Table/utils/types.ts
@@ -1,2 +1,2 @@
-type JsonObjectType = { [key: string]: any };
+export type JsonObjectType = { [key: string]: any };
 export type JsonDataType = string | number | JsonObjectType | Array<JsonObjectType> | boolean;
