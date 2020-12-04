31dedf19e9f85185fdacf19fcb5412c8e59b0e6f
Roman Ignanin <->
2020-12-04 17:08:25 +0300

    Редактирование элементов верхней панели

--- src/containers/TopBar/TopBar.tsx
+++ src/containers/TopBar/TopBar.tsx
@@ -1,34 +1,19 @@
-import { Divider, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
-import Backdrop from '@material-ui/core/Backdrop';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 
-import { QueryField } from '@components/TableModify/QueryField';
 import { useStyles } from '@containers/TopBar/styles';
 
 export const TopBar: React.FC = () => {
   const classes = useStyles();
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div className={classes.root}>
-      <Backdrop className={classes.backdrop} open={open} onClick={handleDrawerClose} />
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -36,14 +21,12 @@ export const TopBar: React.FC = () => {
             className={classes.menuButton}
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             Открыть дерево
           </Typography>
-          <QueryField />
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
@@ -59,32 +42,6 @@ export const TopBar: React.FC = () => {
           </div>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <Typography variant="h5">
-            Меню
-          </Typography>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Таблица', 'Дерево'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
     </div>
   );
 };
