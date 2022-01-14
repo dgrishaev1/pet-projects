56afe2616cc5e3adde4af0f84371222437575537
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2022-01-14 11:28:13 +0300

    async api mock added

--- src/async.test.js
+++ src/async.test.js
@@ -1,5 +1,17 @@
+// api mock
+const fetchData = (success) => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (success) {
+                resolve('peanut butter');
+            } else {
+                reject('error');
+            }
+        }, 100);
+    });
+
+
 // will wait until the done callback is called before finishing the test
-test('the data is peanut butter', done => {
+test('callback: the data is peanut butter', done => {
     function callback(data) {
         try {
             expect(data).toBe('peanut butter');
@@ -9,37 +21,37 @@ test('the data is peanut butter', done => {
         }
     }
 
-    fetchData(callback);
+    fetchData(true).then(callback);
 });
 
 // Promises
-test('the data is peanut butter', () => {
-    return fetchData().then(data => {
+test('promise: the data is peanut butter', () => {
+    return fetchData(true).then(data => {
         expect(data).toBe('peanut butter');
     });
 });
 
-test('the fetch fails with an error', () => {
+test('promise: the fetch fails with an error', () => {
     expect.assertions(1);
     return fetchData().catch(e => expect(e).toMatch('error'));
 });
 
 // .resolves / .rejects
-test('the data is peanut butter', () => {
-    return expect(fetchData()).resolves.toBe('peanut butter'); // if rejected = test failed
+test('res/rej: the data is peanut butter', () => {
+    return expect(fetchData(true)).resolves.toBe('peanut butter'); // if rejected = test failed
 });
 
-test('the fetch fails with an error', () => {
+test('res/rej: the fetch fails with an error', () => {
     return expect(fetchData()).rejects.toMatch('error'); // if resolved = test failed
 });
 
 // async / await
-test('the data is peanut butter', async () => {
-    const data = await fetchData();
+test('async/await: the data is peanut butter', async () => {
+    const data = await fetchData(true);
     expect(data).toBe('peanut butter');
 });
 
-test('the fetch fails with an error', async () => {
+test('async/await: the fetch fails with an error', async () => {
     expect.assertions(1);
     try {
         await fetchData();
@@ -50,10 +62,10 @@ test('the fetch fails with an error', async () => {
 
 // combined with resolves / rejects
 // syntactic sugar for line 15-25 (Promises)
-test('the data is peanut butter', async () => {
-    await expect(fetchData()).resolves.toBe('peanut butter');
+test('await res/rej: the data is peanut butter', async () => {
+    await expect(fetchData(true)).resolves.toBe('peanut butter');
 });
 
-test('the fetch fails with an error', async () => {
+test('await res/rej: the fetch fails with an error', async () => {
     await expect(fetchData()).rejects.toMatch('error');
 });
