59e97bec2b0a4e9f284fdec88dcc89cc59fb208b
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2023-10-27 12:08:59 +0300

    Refac: App component const

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,13 +5,13 @@ import Button from "../Button/Button";
 
 const b = block("app");
 
-function App() {
+const App = () => {
   return (
     <div className={b()}>
       <h1>ui-kit-rollup</h1>
       <Button>button</Button>
     </div>
   );
-}
+};
 
 export default App;
