2d2fccc6209d9984641e10b54617d467af36d23a
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2024-02-23 02:08:01 +0300

    Feat: роуты READ, UPDATE

--- src/pages/router.js
+++ src/pages/router.js
@@ -5,11 +5,23 @@ import { createRouter, createWebHistory } from "vue-router";
 export const ROUTES = {
   MAIN: "/",
   CREATE: "/create",
+  READ: "/read/:id",
+  UPDATE: "/update/:id",
 };
 
 const routesConfig = [
   { path: ROUTES.MAIN, name: "Главная", component: Main },
   { path: ROUTES.CREATE, name: "Создание публикации", component: Create },
+  {
+    path: ROUTES.READ,
+    name: "Публикация",
+    template: "<div>Публикация</div>",
+  },
+  {
+    path: ROUTES.UPDATE,
+    name: "Редактирование публикации",
+    template: "<div>Редактирование публикации</div>",
+  },
 ];
 
 export const router = createRouter({
