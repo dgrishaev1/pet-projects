c96492824b908da52a0d9ddf06ca4e2e116f2d67
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2024-02-23 02:08:01 +0300

    Набросок

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      posts: new Array(3).fill({
+        title: "Публикация",
+        content: "Содержимое публикации",
+      }),
+    };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld>Vite + Vue</HelloWorld>
+  <header class="card p-4 has-background-white">
+    <div class="is-flex is-justify-content-space-between is-align-items-center">
+      <h1 class="is-size-2">Vue blog</h1>
+      <div><b-button type="is-info">Создать публикацию</b-button></div>
+    </div>
+  </header>
+  <main>
+    <div v-for="post in posts" class="card pt-3 pl-4 pb-4 mt-2">
+      <h1 class="is-size-3 pb-1">Наименование карточки</h1>
+      <div>Содержимое карточки</div>
+    </div>
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
