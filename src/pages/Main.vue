1bedafbdb27a35a641b15f84a7339cb1d91f36ac
dgrishaev1 <dgrishaev1@users.noreply.github.com>
2024-02-23 02:08:01 +0300

    Fix: стили карточки

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <template>
-  <div v-for="post in posts" class="card pt-3 pl-4 pb-4 mt-2">
+  <div v-for="post in posts" class="card mt-2 pt-3 pb-4 px-4">
     <h1 class="is-size-3 pb-1">{{ post.title }}</h1>
     <div>{{ post.content }}</div>
   </div>
